6d5ab53c84f784dcf6fe5c5322faa9459df8ce22
rayzeydmusee <104038467+rayzeydmusee@users.noreply.github.com>
2022-06-15 14:30:16 +0300

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:dmusee721@gmail.com" className="p-text">dmusee721@gmail.com</a>
+          <a href="mailto:dmusee721@gmail.com" className="p-text">ronnyalphamal@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
